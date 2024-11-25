4d64d36c25aa4ae14e7494ff8ed26ad653912e10
Raphael Freitas <raphael.soarescivil@gmail.com>
2024-11-25 13:38:15 -0300

    feat: nest cli executed

--- src/read_data/read_csv_data.ts
+++ src/read_data/read_csv_data.ts
@@ -1,25 +1,35 @@
-import fs from "fs";
+import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import csv from 'csv-parser';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const csvFilePath = path.join(__dirname, '../data/eth.csv');
 
-const results = [];
-
 fs.createReadStream(csvFilePath)
-  .pipe(csv({separator: ';'}))
-  .on('data', (row) => {
-    const obj = {
-      date: new Date(row.timestamp),
-      price: row.close
-    }
-    results.push(obj);
+  .pipe(csv({ separator: ';' }))
+  .on('data', (row: string) => {
+    const keys = [
+      'timeOpen',
+      'timeClose',
+      'timeHigh',
+      'timeLow',
+      'name',
+      'open',
+      'high',
+      'low',
+      'close',
+      'volume',
+      'marketCap',
+      'timestamp',
+    ];
+    const values = Object.values(row)[0]
+      .split('";"')
+      .map((value) => value.replace(/(^"|"$)/g, ''));
+    const parsedRow = keys.reduce((obj, key, index) => {
+      obj[key] = values[index];
+      return obj;
+    }, {});
+    console.log(parsedRow);
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
-    console.log(results);
   });
