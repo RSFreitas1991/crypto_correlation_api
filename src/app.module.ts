8e8b30928b1abbdc6b99b5f2852b9f68c23e59d7
Raphael Freitas <raphael.soarescivil@gmail.com>
2024-11-25 13:42:16 -0300

    feat: created dockerfile with mongodb and currency module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CurrencyModule } from './currency/currency.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CurrencyModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
